37312fe9e4ff1e07bd6c2af870747821ad24fc1a
theWarlock <patrickvito2012@gmail.com>
2023-11-12 19:12:33 +0100

    changed the tidio logins

--- hooks/useChatSupport.ts
+++ hooks/useChatSupport.ts
@@ -10,13 +10,12 @@ export const useChatSupport = () => {
       var s1: any = document.createElement("script"),
         s0: any = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/654b8556f2439e1631ed1a48/1heng1njc";
+      s1.src = "https://embed.tawk.to/6551130e958be55aeaaee4af/1hf2b4sir";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
     })();
   }, []);
 };
-
 
 
