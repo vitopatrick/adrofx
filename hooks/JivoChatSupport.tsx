a1ab3206dd49aa13f910550fd9c3207073fdf89e
theWarlock <patrickvito2012@gmail.com>
2023-11-19 19:01:27 +0100

    made it sync

--- hooks/JivoChatSupport.tsx
+++ hooks/JivoChatSupport.tsx
@@ -3,20 +3,20 @@ import { useEffect } from "react";
 
 const JivoChatSupport = () => {
   useEffect(() => {
-    // Create a script element
-    const script = document.createElement("script");
+    const loadScript = () => {
+      // Create a script element
+      const script = document.createElement("script");
 
-    // Set the JivoChat script source
-    script.src = "https://code.jivosite.com/widget/dMD3QovRAt";
-    script.async = true;
+      // Set the JivoChat script source
+      script.src = "https://code.jivosite.com/widget/dMD3QovRAt";
+      script.async = true;
 
-    // Append the script to the body of the document
-    document.body.appendChild(script);
-
-    // Cleanup function to remove the script on component unmount
-    return () => {
-      document.body.removeChild(script);
+      // Append the script to the body of the document
+      document.body.appendChild(script);
     };
+
+    // Load the JivoChat script synchronously
+    loadScript();
   }, []); // Empty dependency array to run the effect only once on mount
 
   return null; // JivoChat widget will be injected into the DOM, so this component doesn't need to render anything
